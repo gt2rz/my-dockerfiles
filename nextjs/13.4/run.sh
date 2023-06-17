75e29e42579d8b486791926134421322eb8dabf8
Miguel GUtierrez <gt2rz.dev@gmail.com>
2023-06-17 18:33:30 -0500

    update nexttjs/13.4

--- nextjs/13.4/run.sh
+++ nextjs/13.4/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker build -t nextjs:13.4 --target=dev . && docker run -it --rm --name myapp -p 3000:3000 nextjs:13.4
+# docker build -t nextjs:13.4 --target=dev . && docker run -it --rm --name myapp -p 3000:3000 nextjs:13.4
 
 # Run: docker run -it --rm -p 3000:3000 -v $(pwd):/app nextjs:13.4
 # Run: docker run -it --rm -p 3000:3000 -v $(pwd):/app -v /app/node_modules nextjs:13.4
+
+docker compose up -d --build
